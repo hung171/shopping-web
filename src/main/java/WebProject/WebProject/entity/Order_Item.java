bf0b3455931a29778cb59c0d5c20f976fc0f48e7
hung171 <hungnguyen17011911@gmail.com>
2023-10-06 09:45:02 +0700

    update

---
+++ src/main/java/WebProject/WebProject/entity/Order_Item.java
@@ -0,0 +1,31 @@
+package WebProject.WebProject.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data // lombok giúp generate các hàm constructor, get, set v.v.
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "order_item")
+public class Order_Item {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "count")
+	private int count;
+	
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	private Product product;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	private Order order;
+}
